89863d23ff518056a1660a88d266f991842100d3
JohanT <vanya.ua60@gmail.com>
2022-08-25 17:54:25 +0300

    VAR->LET

--- app.js
+++ app.js
@@ -3,27 +3,27 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
-var items = ["Buy food", "Cook Food", "Eat Food"]; 
+let items = ["Buy food", "Cook Food", "Eat Food"]; 
 
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function (req, res) {
-  var today = new Date();
-  var options = {
+  let today = new Date();
+  let options = {
     weekday: "long",
     day: "numeric",
     month: "long"
   };
 
-  var day = today.toLocaleDateString("en-US", options);
+  let day = today.toLocaleDateString("en-US", options);
 
   res.render("list", {kindOfDay: day, newListItems: items});
 });
 
 app.post("/", function(req, res){
-  var item = req.body.newItem;
+  let item = req.body.newItem;
 
   items.push(item);
   
