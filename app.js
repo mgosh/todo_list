c2e754e10f112a42ede566ceba25548be2e5f8fd
JohanT <vanya.ua60@gmail.com>
2022-08-27 00:15:54 +0300

    Node Module Exports_ How to Pass Functions and Data between Files

--- app.js
+++ app.js
@@ -2,9 +2,13 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
+
 const app = express();
-let items = ["Buy food", "Cook Food", "Eat Food"]; 
-let workItems = [];
+
+
+const items = ["Buy food", "Cook Food", "Eat Food"]; 
+const workItems = [];
 
 app.set('view engine', 'ejs');
 
@@ -12,16 +16,11 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 app.get("/", function (req, res) {
-  let today = new Date();
-  let options = {
-    weekday: "long",
-    day: "numeric",
-    month: "long"
-  };
 
-  let day = today.toLocaleDateString("en-US", options);
+  let day = date.getDate();
 
   res.render("list", {listTitle: day, newListItems: items});
+
 });
 
 app.post("/", function(req, res){
@@ -42,11 +41,6 @@ app.get("/work", function(req, res){
   res.render("list", {listTitle: "Work List", newListItems: workItems});
 });
 
-// app.post("/work", function(req,res){
-//   let item = req.body.newItem;
-//   workItems.push(item);
-//   res.redirect("/'work");
-// })
 
 
 app.listen(3000, function () {
---
+++ date.js
@@ -0,0 +1,39 @@
+//jshint esversion:6
+
+exports.getDate = function() {
+
+  const today = new Date();
+  const options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long"
+  };
+
+  return today.toLocaleDateString("en-US", options);
+};
+
+
+
+// module.exports = getDate;
+// function getDate() {
+//   let today = new Date();
+//   let options = {
+//     weekday: "long",
+//     day: "numeric",
+//     month: "long"
+//   };
+
+//   return today.toLocaleDateString("en-US", options);
+// };
+
+
+exports.getDay = function() {
+  const today = new Date();
+  const options = {
+    weekday: "long"
+  };
+
+  return today.toLocaleDateString("en-US", options);
+};
+
+
